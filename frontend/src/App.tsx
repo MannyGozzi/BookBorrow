f83810934e6990142cf1b1301e38eb5131450c03
Manuel Gozzi <mannygozzidev@protonmail.com>
2023-05-14 18:15:52 -0700

    Added animated 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import { Box, Center } from "@chakra-ui/react";
+import FourOFour from "./pages/FourOFour";
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
             <Route path="profile" element={<Profile />} />
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
+            <Route path="*" element={<FourOFour/>} />
           </Routes>
         </ Box>
       </Center>
---
+++ frontend/src/pages/FourOFour.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ThemedHeader from '../components/ThemedHeader'
+import { AbsoluteCenter, Image, VStack, Box, Text } from '@chakra-ui/react'
+import BooBo_logo from '../assets/BooBo_logo.png'
+
+const FourOFour = () => {
+  return (
+    <Box h={'80vh'}>
+      <AbsoluteCenter>
+        <VStack>
+        <Image w={'10rem'} borderRadius={'2xl'} src={BooBo_logo} className='bounce animated'/>
+        <ThemedHeader text={'404'} />
+        <Text fontSize={'lg'} fontFamily={'Poppins'}>We couldn't find what you're looking for</Text>
+        </VStack>
+      </AbsoluteCenter>
+    </Box>
+  )
+}
+
+export default FourOFour
