a7bf59a68e5fbba7a3bc0e0ae0dc37d15f64f8fb
Manuel Gozzi <mannygozzidev@protonmail.com>
2023-05-14 19:08:29 -0700

    Added genre field for filter bar

--- frontend/src/components/FilterBar.tsx
+++ frontend/src/components/FilterBar.tsx
@@ -1,22 +1,23 @@
-import { Box, Center, FormControl, Input, Checkbox, CheckboxGroup, Stack, NumberInput, NumberInputField, HStack, AbsoluteCenter } from '@chakra-ui/react'
+import { Box, Center, FormControl, Input, Checkbox, CheckboxGroup, Stack, Flex,  NumberInput, NumberInputField, HStack, AbsoluteCenter } from '@chakra-ui/react'
 import React from 'react'
 
 const FilterBar = () => {
   return (
-    <Center mt={4}>
-      <FormControl bg={'gray.100'} w={'75%'} rounded={'full'}  h={10} >
-        <AbsoluteCenter>
-          <HStack spacing={5} >
-            <NumberInput>
-              <NumberInputField placeholder='Distance' rounded={'full'}></NumberInputField>
-            </NumberInput>
-            <NumberInput>
-              <NumberInputField placeholder='Rating' rounded={'full'}></NumberInputField>
-            </NumberInput>
+    <Center mt={4} >
+      <Box rounded={'full'} bg={'gray.100'} w={'75%'} minHeight={'12'}>
+      <FormControl>
+            <Flex gap={5} pl={2} pr={2} justifyContent={'center'} pt={1}>
+              <NumberInput>
+                <NumberInputField placeholder='Distance' rounded={'full'}></NumberInputField>
+              </NumberInput>
+              <NumberInput>
+                <NumberInputField placeholder='Rating' rounded={'full'}></NumberInputField>
+              </NumberInput>
               <Checkbox defaultChecked>Hardcover</Checkbox>
-          </HStack>
-        </AbsoluteCenter >
-      </FormControl>
+              <Input type='text' placeholder='Genre' rounded={'full'}/>
+            </Flex>
+        </FormControl>  
+      </Box>
     </Center>
   )
 }
