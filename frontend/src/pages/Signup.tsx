4aa742887dc049ce55e673c453de441671c5a774
Manuel Gozzi <mannygozzidev@protonmail.com>
2023-05-18 19:02:37 -0700

    Fixed signup again

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ThemedHeader from '../components/ThemedHeader'
-import {Signup as SignupComp} from '../components/signup-thing'
+import {Signup as SignupComp} from '../components/Signup'
 
 const Signup = () => {
     return (
