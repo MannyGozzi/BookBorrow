7c24910b4fe2295eaba95b6cd746dbf5022d12e7
Jaron Li <2002jaron@gmail.com>
2023-05-18 18:53:22 -0700

    working on account page

--- frontend/src/components/ProfileInfo.tsx
+++ frontend/src/components/ProfileInfo.tsx
@@ -17,45 +17,32 @@ import {
     Avatar,
     MenuButton,
     Flex,
-    Menu
+    Menu,
+    Card, CardHeader, CardBody, CardFooter, StackDivider
   } from '@chakra-ui/react'
   
   export default function ProfileInfo() {
   return (
-    <Container maxW='700px'>
-      <Flex h={200} alignItems={'center'} justifyContent={'space-between'}>
-      <HStack spacing='30px'>
-      <Box w='100px' h='100px'>
-        <Text fontSize="2xl"><span className='theme-header'>Profile</span></Text>
-      </Box>
-      <Menu>
-        <MenuButton
-          as={Button}
-          rounded={'full'}
-          variant={'link'}
-          cursor={'pointer'}
-          minW={0}>
-          <Avatar
-              size={'md'}
-              src='https://bit.ly/broken-link'
-          />
-        </MenuButton>
-      </Menu>
-      <Box w='100px' h='100px'>
-      <Text fontSize="2xl"><span className='theme-header'>Stars</span></Text>
-      </Box>
-    </HStack>
-    </Flex>
-
-    <VStack spacing='10px'>
-      <Box w='100px' h='100px'>
-        <Text fontSize="2xl"><span className='theme-header'>Email</span></Text>
-      </Box>
-      <Box w='175px' h='100px'>
-      <Text fontSize="2xl"><span className='theme-header'>Phone Number</span></Text>
-      </Box>
-    </VStack>
-    
-    </Container>
+    <Center>
+    {/* <Flex alignItems="center" justifyContent="flex-start"> */}
+      <Container>
+        <Flex alignItems="center" justifyContent="space-between">
+          <HStack spacing='30px'>
+          <Box w='500px' h='100px'>
+              <Text fontSize="2xl"><span className='theme-header'>Profile</span></Text>
+            </Box>
+            <Menu>
+              <MenuButton>
+                <Avatar
+                  size={'md'}
+                  src='https://bit.ly/broken-link'
+                />
+              </MenuButton>
+            </Menu>
+            </HStack>
+            </Flex>
+      </Container>
+     {/* </Flex> */}
+    </Center>
     )
   }
