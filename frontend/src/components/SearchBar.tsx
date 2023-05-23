7504046a1484ee1f5e62af6d27e2093907b29a83
Manuel Gozzi <mannygozzidev@protonmail.com>
2023-05-23 10:25:47 -0700

    Fixed dark mode and styles for major elements

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ import { TextType } from '../types'
 const SearchBar = ({text} : TextType) => {
   return (
     <Center mt={4}>
-        <Box w={'75%'}>
+        <Box w={{lg: '75%', md: '85%', sm: '100%'}}>
         <FormControl>
         <Input type='email' placeholder={text} rounded={'full'}/>
         </FormControl>
