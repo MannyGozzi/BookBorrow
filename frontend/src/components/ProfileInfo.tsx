d7dda6ee2982153a73d77c6f2b3e9e8e66991300
Jaron Li <2002jaron@gmail.com>
2023-06-06 17:51:05 -0700

    stars and number of reviews show

--- frontend/src/components/ProfileInfo.tsx
+++ frontend/src/components/ProfileInfo.tsx
@@ -31,7 +31,7 @@ export default function ProfileInfo({userid, isLocalUser} : {userid: string, isL
     axios.get(`http://localhost:3000/users/${userid}`)
     .then(res => {
         setUser(res.data.user)
-        calculateAverageRating();
+        getRating();
     })
     .catch(err => console.log(err.message))
 
@@ -44,49 +44,22 @@ export default function ProfileInfo({userid, isLocalUser} : {userid: string, isL
       axios.get(`http://localhost:3000/checkout/from/${userid}`, {withCredentials: true})
 }, [dispatch, userid])
 
-//   const getRating = async () => {
-//   axios.get(`http://localhost:3000/reviews/${_id}`,
-//     { withCredentials: true })
-//     .then(response => {
-//       let avgRating = 0
-//       if (response.data) {
-//         avgRating = response.data.reduce((acc: number, review: any) => acc + review.rating, 0)
-//         avgRating /= Math.max(response.data.length, 1)
-//       }
-//       setRating(avgRating)
-//     })
-//     .catch(error => console.error(error))
-// }
-  // const calculateAverageRating = (reviews: IReview[]) => {
-  const calculateAverageRating = async () => {
-    axios.get(`http://localhost:3000/reviews/${currentUser?._id}`,
-    { withCredentials: true })
-    .then(response => {
-      let avgRating = 0
-      if (response.data) {
-        avgRating = response.data.reduce((acc: number, review: any) => acc + review.rating, 0)
-        avgRating /= Math.max(response.data.length, 1)
-      }
-      setAverageRating(avgRating)
-    })
-    .catch(error => console.error(error))
-    // if (!reviews) return
-    // if (reviews.length === 0) {
-    //   setAverageRating(0);
-    //   return;
-    // }
-
-    // let totalRating = 0;
-    // let i = 0
-    // for (i; i < reviews.length; i++) {
-    //   totalRating += reviews[i].rating;
-    // }
-
-    // const totalReviews = i;
-    // const average = totalRating / reviews.length;
-    // setAverageRating(average);
-    // setNumRatings(totalReviews)
-  }
+  const getRating = async () => {
+    axios
+      .get(`http://localhost:3000/reviews/${userid}`, { withCredentials: true })
+      .then(response => {
+        let avgRating = 0;
+        let numReviews = 0;
+        if (response.data) {
+          avgRating = response.data.reduce((acc: number, review: any) => acc + review.rating, 0);
+          numReviews = response.data.length;
+          avgRating /= Math.max(numReviews, 1);
+        }
+        setAverageRating(avgRating);
+        setNumRatings(numReviews);
+      })
+      .catch(error => console.error(error));
+  };
   
   return (
     <Center m={4}>
@@ -110,7 +83,7 @@ export default function ProfileInfo({userid, isLocalUser} : {userid: string, isL
               <Spacer />
               <VStack spacing="5px" align="flex-end">
                 <StarRating rating={averageRating}/>
-                <Text fontSize="xs">(Number of Ratings: {numRatings})</Text>
+                <Text fontSize="xs">(Number of Reviews: {numRatings})</Text>
               </VStack>
             </HStack>
           </Box>
