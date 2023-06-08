6352ee6aef5bbac55f94b10d3bd5f5ddaee58877
Jaron Li <2002jaron@gmail.com>
2023-06-07 23:29:44 -0700

    think im done displaying reviews

--- frontend/src/components/Review.tsx
+++ frontend/src/components/Review.tsx
@@ -15,8 +15,22 @@ import {
   import { useEffect, useState } from 'react';
   import { IReview } from '../types';
   import { CalendarIcon } from '@chakra-ui/icons';
+  import { IUser } from '../types';
+  import { useDispatch } from 'react-redux'
   
   function Review({ _id, reviewer, reviewed, rating, comment, date_created}: IReview) {  
+
+    const [currentUser, setCurrentUser] = useState<IUser>();
+    const dispatch = useDispatch()
+    
+    useEffect(() => {
+      axios.get(`http://localhost:3000/users/${reviewer}`)
+      .then(res => {
+          setCurrentUser(res.data.user)
+      })
+      .catch(err => console.log(err.message))
+    }, [dispatch, _id])
+
     return (
         <Center>
           <Box
@@ -38,17 +52,19 @@ import {
                   mt={0}
                   mb={2}
                 >
-                  {reviewer}
+                  {currentUser?.first_name} {currentUser?.last_name}
                 </Heading>
                 <HStack className="content-center" mb={2}>
                   {Array.from({ length: rating }).map((_, index) => (
                     <FaStar key={index} color={'yellow.400'} />
                   ))}
                 </HStack>
-    
+                  
                 <Text color={'gray.500'} fontFamily={'body'} mb={4}>
                   {comment}
                 </Text>
+                      </Box>
+                <Box >
                 <Text fontFamily={'Poppins'}>
                   <CalendarIcon mx={2} /> {new Date(date_created).toDateString()}
                 </Text>
@@ -61,44 +77,3 @@ import {
     
   
   export default Review;
-
-// import { Box, Heading, Text, useColorModeValue, HStack, Icon } from '@chakra-ui/react';
-// import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
-// import { CalendarIcon } from '@chakra-ui/icons';
-// import { IReview } from '../types';
-
-// function Review({ reviewer, rating, comment, date_created }: IReview) {
-//   return (
-//     <Box
-//       bg={useColorModeValue('white', 'gray.700')}
-//       boxShadow={'xl'}
-//       rounded={'xl'}
-//       p={3}
-//       mb={4}
-//     >
-//       <Heading
-//         color={useColorModeValue('gray.600', 'white')}
-//         fontSize={'xl'}
-//         fontFamily={'Poppins'}
-//         mt={0}
-//         mb={2}
-//       >
-//         {reviewer}
-//       </Heading>
-//       <HStack className="content-center" mb={2}>
-//         {Array.from({ length: rating }).map((_, index) => (
-//           <FaStar key={index} color={'yellow.400'} />
-//         ))}
-//       </HStack>
-
-//       <Text color={'gray.500'} fontFamily={'body'} mb={4}>
-//         {comment}
-//       </Text>
-//       <Text fontFamily={'Poppins'}>
-//         <Icon as={CalendarIcon} mx={2} /> {new Date(date_created).toDateString()}
-//       </Text>
-//     </Box>
-//   );
-// }
-
-// export default Review;
